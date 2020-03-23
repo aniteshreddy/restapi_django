247cdcd31b726eb8ea5e1857a83ac93f40a8e673
aniteshreddy <reddyanitesh@gmail.com>
2020-03-23 17:06:22 +0530

    post gres

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = "tutorial.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "anitesh",
-        "USER": "aniteshreddy",
-        "PASSWORD": "star@131",
-        "HOST": "localhost",
+        "ENGINE": "django.db.backends.sqlite.db",
+        "NAME":os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
